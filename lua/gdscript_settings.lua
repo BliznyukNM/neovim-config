8ffeb8f6b405e51284c79d5b2eb6548860c736b4
Nikita Bliznyuk <bliznyuknm@gmail.com>
2021-10-12 19:58:32 +0200

    removed old files

--- lua/gdscript_settings.lua
+++
@@ -1,5 +0,0 @@
-godot_executable = '/Users/bliznyuknm/Applications/Godot 3.4.x.app'
-nvim_lsp.gdscript.setup { coq.lsp_ensure_capabilities {
-    capabilities = capabilities,
-    on_attach = on_attach
-}}
