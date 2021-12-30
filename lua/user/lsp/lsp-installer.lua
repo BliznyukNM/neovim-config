108d605fae7a11eb80451a007e08f246b42e268f
Nikita Blizniuk <nikita.blinzyuk@tensquaregames.com>
2021-12-30 13:36:26 +0100

    added jdtls

--- lua/user/lsp/lsp-installer.lua
+++ lua/user/lsp/lsp-installer.lua
@@ -26,6 +26,11 @@ lsp_installer.on_server_ready(function(server)
 	 	opts = vim.tbl_deep_extend("force", omnisharp_opts, opts)
 	 end
 
+   if server.name == "jdtls" then
+     local jdtls_opts = require("user.lsp.settings.jdtls")
+     opts = vim.tbl_deep_extend("force", jdtls_opts, opts)
+   end
+
 	-- This setup() function is exactly the same as lspconfig's setup function.
 	-- Refer to https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
 	server:setup(opts)
---
+++ lua/user/lsp/settings/jdtls.lua
@@ -0,0 +1,3 @@
+return {
+  cmd = { vim.fn.expand('$HOME/.config/nvim/java-lsp.sh') }
+}
