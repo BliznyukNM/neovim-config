a9d2ee8a4e942a836d5f22dbde2b3bd75ccdfb3c
Nikita Blizniuk <nikita.bliznyuk@tensquaregames.com>
2022-06-07 12:18:37 +0200

    fixed omnisharp

--- lua/user/lsp/settings/omnisharp.lua
+++ lua/user/lsp/settings/omnisharp.lua
@@ -1,6 +1,7 @@
-local omnisharp_bin = vim.fn.expand "$HOME/.local/share/nvim/lsp_servers/omnisharp/omnisharp/omnisharp/OmniSharp.exe"
+local omnisharp_bin = vim.fn.expand "$HOME/.local/share/nvim/lsp_servers/omnisharp/omnisharp/OmniSharp"
 local pid = vim.fn.getpid()
 
 return {
-  cmd = { "mono", omnisharp_bin, "--languageserver", "--hostPID", tostring(pid) },
+  cmd = { omnisharp_bin, "--languageserver", "--hostPID", tostring(pid) },
+  use_mono = true
 }
