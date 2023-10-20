2e22d91c2bee35e3007faa8865b370480b9e4bed
Nikita Blizniuk <bliznyuknm@gmail.com>
2023-10-20 20:21:51 +0200

    set fish as toggleterm terminal

--- lua/user/toggleterm.lua
+++ lua/user/toggleterm.lua
@@ -15,7 +15,7 @@ toggleterm.setup({
 	persist_size = true,
 	direction = "float",
 	close_on_exit = true,
-	shell = vim.o.shell,
+	shell = "/opt/homebrew/bin/fish",
 	float_opts = {
 		border = "curved",
 		winblend = 0,
