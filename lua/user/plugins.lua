33834d2fd8c417eb177c2f06ede5137d19903097
Nikita Blizniuk <bliznyuknm@gmail.com>
2023-10-20 19:55:05 +0200

    remove redundant plugin

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -87,7 +87,6 @@ return packer.startup(function(use)
   use "williamboman/nvim-lsp-installer" -- simple to use language server installer
   use "tamago324/nlsp-settings.nvim" -- language server settings defined in json for
   use "jose-elias-alvarez/null-ls.nvim" -- for formatters and linters
-  use "BliznyukNM/nim.nvim" -- nim plugin for LSP
   use 'williamboman/mason.nvim' -- Installer for LSP and DAP servers
   use 'williamboman/mason-lspconfig.nvim' -- LSP wrapper
   use "jay-babu/mason-null-ls.nvim"
